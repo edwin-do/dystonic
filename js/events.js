d9d7a2d08a275295298732db508b72fa60cfa141
edwin-do <doedwin123@gmail.com>
2020-10-03 18:45:07 -0400

    add keydown event

--- js/events.js
+++ js/events.js
@@ -1,3 +1,11 @@
+var imagesArray = ["brother.jpg", "grandchild1.jpg", "sister.jpg"];
+var imagesAlt = ["brother", "grandchild1", "sister"];
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+    console.log(e.code);
+  }
 
 function revealAnswer(id){
         var img = document.getElementById(id.toString());
@@ -6,6 +14,7 @@ function revealAnswer(id){
           img.style.display = "block";
         } else {
           img.style.display = "none";
+          ans.style.display = "block";
           ans.textContent = img.alt.toString();
         }
       }
@@ -20,12 +29,13 @@ function hideImage(){
           img.style.display = "none";
       }
 
-
-//   $( document ).ready(function() {
-//     var menu = $('#menu');
-//     var myNav = $('#myNav');
-//     menu.click(function() {
-//         myNav.toggleClass('open');
-//        return false;
-//     });
-//   });
+function newImage(){
+        var img = document.getElementById('q-img');
+        var ans = document.getElementById("answer");
+        var num = Math.floor(Math.random() * 3);
+        img.src = "./img/photos/" + imagesArray[num];
+        img.alt = imagesAlt[num];
+        img.style.display = "block";
+        ans.textContent = img.alt.toString();
+        ans.style.display = "none";
+      }
