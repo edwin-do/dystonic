1e5425ddc9da12725a32605053d10627f6d70de9
edwin-do <doedwin123@gmail.com>
2020-10-03 22:21:24 -0400

    add keyboard events

--- js/events.js
+++ js/events.js
@@ -4,11 +4,25 @@ var imagesAlt = ["brother", "grandchild1", "sister"];
 document.addEventListener('keydown', logKey);
 
 function logKey(e) {
-    console.log(e.code);
+    if (e.code == "ArrowRight" | e.code == "ArrowLeft"){
+        newImage();
+    }
+    else if(e.code == "Space"){
+        revealAnswer();
+    }
   }
 
-function revealAnswer(id){
-        var img = document.getElementById(id.toString());
+function hideIntro(){
+    var intro = document.getElementById('intro');
+    intro.style.display = "none";
+    var btnNav = document.getElementById('btn-nav');
+    btnNav.textContent = "Next";
+
+
+}
+
+function revealAnswer(){
+        var img = document.getElementById("q-img");
         var ans = document.getElementById("answer");
         if (img.style.display === "none") {
           img.style.display = "block";
@@ -30,12 +44,16 @@ function hideImage(){
       }
 
 function newImage(){
+    // Select and display new random image 
         var img = document.getElementById('q-img');
         var ans = document.getElementById("answer");
         var num = Math.floor(Math.random() * 3);
         img.src = "./img/photos/" + imagesArray[num];
         img.alt = imagesAlt[num];
         img.style.display = "block";
+
+    // assign new answer to alt and hide it 
         ans.textContent = img.alt.toString();
         ans.style.display = "none";
+        hideIntro();
       }
